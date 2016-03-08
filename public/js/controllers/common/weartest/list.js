176429863cdca45ae0602a6940dfc3c8f05e33fc
root <root@positive.(none)>
2016-03-08 19:59:13 +0800

    new change

--- public/js/controllers/common/weartest/list.js
+++ public/js/controllers/common/weartest/list.js
@@ -14,6 +14,7 @@ function ($scope, $location, $http, WTServices, async, notificationWindow) {
             'imageSetId': 1,
             'identification': 1,
             'name': 1,
+            'rating':1,
             'brand': 1,
             'registrationStart': 1,
             'registrationDeadline': 1,
